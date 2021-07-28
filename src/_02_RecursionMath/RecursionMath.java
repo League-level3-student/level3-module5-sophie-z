39857dce411285583a0a54b0e65e212b45613475
sophie-z <sophie.zeng2@gmail.com>
2021-07-27 18:42:57 -0700

    ss

--- src/_02_RecursionMath/RecursionMath.java
+++ src/_02_RecursionMath/RecursionMath.java
@@ -4,24 +4,35 @@ public class RecursionMath {
 
     public static int recursiveMultiplication(int number, int times) {
         // If times is 1 
+    	if(times == 1) {
             // Return number 
-
+    		return number;
+    	}
         // Else return number + recursionMultiplication(number, times-1)
-
-        return 0;
+    	else {
+    		return number + recursiveMultiplication(number, times-1);
+    	}
     }
 
     // Try this one on your own! 
     // Hint: if numberToDivideBy is bigger than number,
     //       you can't divide anymore
-    public static int recursiveDivision(int number, int numberToDevideBy) {
-        
-        return 0;
+    public static int recursiveDivision(int number, int numberToDivideBy) {
+        if(numberToDivideBy>number) {
+        	return 0;
+        }
+        else {
+        	return 1+recursiveDivision(number-numberToDivideBy, numberToDivideBy);
+        }
     }
-
     // Try this one on your own!
     public static int recursivePower(int number, int power) {
-        
-        return 0;
+        if(power == 1) {
+        	return number;
+        }
+        else {
+        	return number*recursivePower(number, power-1);
+        }
+
     }
 }
--- src/_02_RecursionMath/RecursionMathTest.java
+++ src/_02_RecursionMath/RecursionMathTest.java
@@ -10,20 +10,23 @@ class RecursionMathTest {
     @Test
     public void testMultiplication() {
         assertEquals(12, RecursionMath.recursiveMultiplication(3, 4));
-        
         // Add more JUnit tests like the one above to test your method
-        
+        assertEquals(36, RecursionMath.recursiveMultiplication(6, 6));
+        assertEquals(100, RecursionMath.recursiveMultiplication(1, 100));
     }
 
     @Test
     public void testDivision() {
         // Add JUnit tests to test your method
-        fail("Write some tests!");
+    	assertEquals(2, RecursionMath.recursiveDivision(10, 5));
+    	assertEquals(50, RecursionMath.recursiveDivision(100, 2));
+    	assertEquals(0, RecursionMath.recursiveDivision(10, 20));
     }
 
     @Test 
     public void testPower() {
         // Add JUnit tests to test your method
-        fail("Write some tests!");
+        assertEquals(8, RecursionMath.recursivePower(2, 3));
+        assertEquals(25, RecursionMath.recursivePower(5, 2));
     }
 }
